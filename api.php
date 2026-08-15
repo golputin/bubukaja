e8a2c08d18c0d488c02aa872a19b435244600241
golputin <golputinsvy@gmail.com>
2026-08-16 01:57:34 +0700

    Add files via upload

---
+++ api.php
@@ -0,0 +1,29 @@
+{
+    "success": true,
+    "data": {
+        "original": {
+            "payload": "00020101021126570011ID.DANA.WWW011893600915398890465902099889046590303UMI51440014ID.CO.QRIS.WWW0215ID10254323269040303UMI5204549953033605802ID5914ARJUNA BINTANG6013Kota Parepare6105911256304D259",
+            "merchant_name": "ARJUNA BINTANG",
+            "city_name": "Kota Parepare",
+            "postal_code": "91125",
+            "nmid": "ID1025432326904",
+            "is_dynamic": false,
+            "amount": 0
+        },
+        "modified": {
+            "payload": "00020101021126570011ID.DANA.WWW011893600915398890465902099889046590303UMI51440014ID.CO.QRIS.WWW0215ID10254323269040303UMI5204549953033605802ID5914ARJUNA BINTANG6013Kota Parepare6105911256304D259",
+            "merchant_name": "ARJUNA BINTANG",
+            "city_name": "Kota Parepare",
+            "postal_code": "91125",
+            "nmid": "ID1025432326904",
+            "is_dynamic": false,
+            "amount": 0,
+            "base_amount": 0,
+            "service_fee": 0,
+            "total_amount": 0,
+            "fee_type": "none",
+            "fee_value": 0
+        },
+        "qr_code_url": "https:\/\/api.qrserver.com\/v1\/create-qr-code\/?size=500x500&margin=10&data=00020101021126570011ID.DANA.WWW011893600915398890465902099889046590303UMI51440014ID.CO.QRIS.WWW0215ID10254323269040303UMI5204549953033605802ID5914ARJUNA+BINTANG6013Kota+Parepare6105911256304D259"
+    }
+}
